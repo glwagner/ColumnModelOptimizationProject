b6ed86b334f1674757436d74e5c5b415cbf5be1f
Gregory Wagner <wagner.greg@gmail.com>
2019-08-20 17:24:14 -0400

    updates

--- les/deepening_mixed_layer.jl
+++ les/deepening_mixed_layer.jl
@@ -35,10 +35,8 @@ model = Model(      arch = HAVE_CUDA ? GPU() : CPU(),
 
 # Set initial condition. Initial velocity and salinity fluctuations needed for AMD.
 Ξ(z) = randn() * z / model.grid.Lz * (1 + z / model.grid.Lz) # noise
-θᵢ(x, y, z) = 20 + dθdz * z + dθdz * model.grid.Lz * 1e-6 * Ξ(z)
-uᵢ(x, y, z) = 1e-4 * Ξ(z)
-
-set!(model, u=uᵢ, v=uᵢ, T=θᵢ, S=uᵢ)
+θᵢ(x, y, z) = 20 + dθdz * z
+set!(model, T=θᵢ)
 
 T_gpu = CuArray{Float64}(undef, 1, 1, model.grid.Tz)
 
@@ -56,14 +54,10 @@ wizard = TimeStepWizard(cfl=0.05, Δt=0.05, max_change=1.1, max_Δt=90.0)
 # Set up output
 #
 #
-const VAMD = VerstappenAnisotropicMinimumDissipation
-const CID = ConstantIsotropicDiffusivity
-
-closurename(::CID) = "DNS"
-closurename(closure::VAMD) = @sprintf("AMD_C%.2f", closure.C)
 
 function init_bcs(file, model)
     file["boundary_conditions/top/Qb"] = Qb
+    file["boundary_conditions/top/Qθ"] = Qθ
     file["boundary_conditions/top/Qu"] = Qu
     file["boundary_conditions/bottom/N²"] = N²
     return nothing
@@ -77,8 +71,7 @@ T(model) = Array(model.tracers.T.data.parent)
 κₑ(model) = Array(model.diffusivities.κₑ.T.data.parent)
 
 fields = Dict(:u=>u, :v=>v, :w=>w, :T=>T, :ν=>νₑ, :κ=>κₑ)
-filename = @sprintf("%s_Nx%d_Nz%d_cfl%.2f_%s", case, Nx, Nz, wizard.cfl, 
-                    closurename(model.closure))
+filename = @sprintf("%s_Nx%d_Nz%d_cfl%.2f_amd_C%.2f%s", case, Nx, Nz, wizard.cfl, model.closure.C)
 field_writer = JLD2OutputWriter(model, fields; dir="data", init=init_bcs, prefix=filename, 
                                 max_filesize=1GiB, interval=6hour, force=true)
 push!(model.output_writers, field_writer)
@@ -94,26 +87,18 @@ function terse_message(model, walltime, Δt)
                     model.clock.iteration, model.clock.time/3600, Δt, wmax, cfl, prettytime(walltime))
 end
 
-
-w²_filename = @sprintf("data/vertical_velocity_variance_%s_Nx%d_Nz%d.jld2", case, Nx, Nz)
-max_w², t = [], []
-include("velocity_variance_utils.jl")
-
 # Spin up
 for i = 1:100
     update_Δt!(wizard, model)
-    walltime = Base.@elapsed step_with_w²!(max_w², t, model, wizard.Δt, 10)
+    walltime = Base.@elapsed time_step!(model, 10, wizard.Δt)
     @printf "%s" terse_message(model, walltime, wizard.Δt)
 end
 
 # Run the model
 while model.clock.time < tf
     update_Δt!(wizard, model)
-    walltime = Base.@elapsed step_with_w²!(max_w², t, model, wizard.Δt, 100)
+    walltime = Base.@elapsed time_step!(model, 10, wizard.Δt)
     @printf "%s" terse_message(model, walltime, wizard.Δt)
-
-    rm(w²_filename, force=true)
-    @save w²_filename max_w² t
 
     if model.clock.iteration % 10000 == 0
         plt = plot_average_temperature(model)
