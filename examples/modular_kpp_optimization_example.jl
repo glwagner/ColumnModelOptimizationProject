14b5fbf55097126eec63d82804f3ac2b1afa781e
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2020-02-01 16:07:02 -0500

    Updates to example script

--- examples/modular_kpp_optimization_example.jl
+++ examples/modular_kpp_optimization_example.jl
@@ -13,11 +13,10 @@ model = ModularKPPOptimization.ColumnModel(data, 5minute, N=64,
                                            kprofile=ModularKPP.GeneralizedCubicPolynomial())
 
 default_parameters = DefaultFreeParameters(model, WindMixingAndShapeParameters)
-standard_deviation = Array(1e-1 * default_parameters)
+standard_deviation = [1e-1 for i = 1:length(default_parameters)]
 bounds = [(0.0, 2.0), (0.0, 1.0), (0.0, 10.0), (0.0, 1.0), (0.0, 1.0)]
 
-#loss_function = TimeAveragedLossFunction(targets=11:10:201, fields=(:U, :V, :T), weights=(0.0, 0.0, 0.1))
-loss_function = TimeAveragedLossFunction(targets=11:10:201, fields=:T)
+loss_function = TimeAveragedLossFunction(data, targets=11:length(data), fields=(:U, :V, :T))
 
 nll = NegativeLogLikelihood(model, data, loss_function)
 
@@ -29,44 +28,12 @@ iterations = 10
 initial_steps = 25
 sampler = MetropolisSampler(BoundedNormalPerturbation(standard_deviation, bounds))
 initial_link = MarkovLink(nll, default_parameters)
-@time chain = MarkovChain(100, initial_link, nll, sampler)
+
+covariance, chain = estimate_covariance(nll, default_parameters, standard_deviation, 
+                                        BoundedNormalPerturbation, bounds, samples=100, niterations=10)
+
+@show optimal(chain)
+@show covariance
+
 visualize_realizations(model, data, loss_function.targets[[1, length(loss_function.targets)]], 
                        chain[1].param, optimal(chain).param)
-
-#=
-for i = 1:iterations
-
-    global chain
-    global covariance
-
-    if i == 1
-        initial_link = MarkovLink(nll, default_parameters)
-        sampler = MetropolisSampler(BoundedNormalPerturbation(standard_deviation, bounds))
-        @show nll.scale = initial_link.error
-        initial_link = MarkovLink(nll, default_parameters)
-    else
-        optimal_link = optimal(chain)
-        sampler = MetropolisSampler(BoundedNormalPerturbation(covariance, bounds))
-        @show nll.scale = optimal_link.error
-        initial_link = MarkovLink(nll, optimal_link.param)
-    end
-
-    @time chain = MarkovChain(i * initial_steps, initial_link, nll, sampler)
-    parameter_matrix = zeros(3, length(chain))
-
-    for (i, link) in enumerate(chain.links)
-        parameter_matrix[:, i] .= link.param
-    end
-
-    @show covariance = cov(parameter_matrix, dims=2)
-    @show optimal(chain).error / nll.scale
-
-end
-=#
-
-#=
-@show chain.acceptance
-@show optimal(chain)
-
-visualize_realizations(model, data, loss_function.targets[[1, 401]], chain[1].param, optimal(chain).param)
-=#
