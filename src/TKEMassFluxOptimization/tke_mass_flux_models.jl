eb1660216d8f008f914e130886f8bf430120f06d
Adeline Hillier <adelineh2000@gmail.com>
2021-02-17 00:26:15 -0800

    Make compatible with LESbrary.jl/examples/three_layer_example.jl data

--- src/TKEMassFluxOptimization/tke_mass_flux_models.jl
+++ src/TKEMassFluxOptimization/tke_mass_flux_models.jl
@@ -4,15 +4,30 @@ function ColumnModel(cd::ColumnData, Δt; Δz=nothing, N=nothing, kwargs...)
         N = ceil(Int, cd.grid.H / Δz)
     end
 
-    model = simple_tke_model(cd.constants; 
-                                N = N, 
+    # model = simple_tke_model(cd.constants;
+    #                             N = N,
+    #                             H = cd.grid.H,
+    #                            Qᶿ = cd.surface_fluxes.Qᶿ,
+    #                            Qˢ = cd.surface_fluxes.Qˢ,
+    #                            Qᵘ = cd.surface_fluxes.Qᵘ,
+    #                            Qᵛ = cd.surface_fluxes.Qᵛ,
+    #                          dTdz = cd.initial_conditions.dTdz,
+    #                          dSdz = cd.initial_conditions.dSdz,
+    #                          kwargs...
+    #                         )
+
+    # Uᵢ = cd.U[:,1]
+    # Vᵢ = cd.V[:,1]
+    # Tᵢ = cd.T[:,1]
+
+    model = simple_tke_model(cd.constants;
+                                N = N,
                                 H = cd.grid.H,
-                               Qᶿ = cd.surface_fluxes.Qᶿ, 
-                               Qˢ = cd.surface_fluxes.Qˢ,
-                               Qᵘ = cd.surface_fluxes.Qᵘ, 
-                               Qᵛ = cd.surface_fluxes.Qᵛ,
-                             dTdz = cd.initial_conditions.dTdz, 
-                             dSdz = cd.initial_conditions.dSdz, 
+                               Qᶿ = cd.boundary_conditions.Qᶿ,
+                               Qᵘ = cd.boundary_conditions.Qᵘ,
+                      dθdz_bottom = cd.boundary_conditions.dθdz_bottom,
+                      dudz_bottom = cd.boundary_conditions.dudz_bottom,
+              initial_temperature = cd.initial_conditions.initial_temperature,
                              kwargs...
                             )
 
@@ -38,17 +53,14 @@ bottom temperature gradient `dTdz`, and forced by
 The keyword arguments `diffusivity`, `mixingdepth`, nonlocalflux`, and `kprofile` set
 their respective components of the `OceanTurb.TKEMassFlux.Model`.
 """
-function simple_tke_model(constants=Constants(); 
-                          N = 128, 
-                          H, 
-                          dTdz, 
-                          dSdz,
-                          Qᶿ = 0.0, 
-                          Qˢ = 0.0, 
-                          Qᵘ = 0.0, 
-                          Qᵛ = 0.0, 
-                          T₀ = 20.0, 
-                          S₀ = 35.0,
+function simple_tke_model(constants=Constants();
+                          N = 128,
+                          H,
+                          Qᶿ = 0.0,
+                          Qᵘ = 0.0,
+                 dθdz_bottom = 0.0,
+                 dudz_bottom = 0.0,
+         initial_temperature = initial_temperature,
                           kwargs...
                          )
 
@@ -58,22 +70,63 @@ function simple_tke_model(constants=Constants();
                                   kwargs...
                              )
 
-    # Initial condition
-    Tᵢ(z) = T₀ + dTdz * z
-    Sᵢ(z) = S₀ + dSdz * z
 
-    model.solution.T = Tᵢ
-    model.solution.S = Sᵢ
+    # Initial condition
+    # model.solution.U = Uᵢ
+    # model.solution.V = Vᵢ
+    # model.solution.T = Tᵢ
+
+    model.solution.T = initial_temperature
 
     # Surface fluxes
     model.bcs.U.top = FluxBoundaryCondition(Qᵘ)
-    model.bcs.V.top = FluxBoundaryCondition(Qᵛ)
     model.bcs.T.top = FluxBoundaryCondition(Qᶿ)
-    model.bcs.S.top = FluxBoundaryCondition(Qˢ)
+    # model.bcs.S.top = FluxBoundaryCondition(Qˢ)
 
     # Bottom gradients
-    model.bcs.T.bottom = GradientBoundaryCondition(dTdz)
-    model.bcs.S.bottom = GradientBoundaryCondition(dSdz)
+    model.bcs.T.bottom = GradientBoundaryCondition(dθdz_bottom)
+    model.bcs.U.bottom = GradientBoundaryCondition(dudz_bottom)
+    # model.bcs.S.bottom = GradientBoundaryCondition(dSdz)
 
     return model
 end
+
+# function simple_tke_model(constants=Constants();
+#                           N = 128,
+#                           H,
+#                           dTdz,
+#                           dSdz,
+#                           Qᶿ = 0.0,
+#                           Qˢ = 0.0,
+#                           Qᵘ = 0.0,
+#                           Qᵛ = 0.0,
+#                           T₀ = 20.0,
+#                           S₀ = 35.0,
+#                           kwargs...
+#                          )
+#
+#     model = TKEMassFlux.Model(;     grid = UniformGrid(N=N, H=H),
+#                                constants = constants,
+#                                  stepper = :BackwardEuler,
+#                                   kwargs...
+#                              )
+#
+#     # Initial condition
+#     Tᵢ(z) = T₀ + dTdz * z
+#     Sᵢ(z) = S₀ + dSdz * z
+#
+#     model.solution.T = Tᵢ
+#     model.solution.S = Sᵢ
+#
+#     # Surface fluxes
+#     model.bcs.U.top = FluxBoundaryCondition(Qᵘ)
+#     model.bcs.V.top = FluxBoundaryCondition(Qᵛ)
+#     model.bcs.T.top = FluxBoundaryCondition(Qᶿ)
+#     model.bcs.S.top = FluxBoundaryCondition(Qˢ)
+#
+#     # Bottom gradients
+#     model.bcs.T.bottom = GradientBoundaryCondition(dTdz)
+#     model.bcs.S.bottom = GradientBoundaryCondition(dSdz)
+#
+#     return model
+# end
