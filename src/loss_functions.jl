42406b858f30fcc547c4499f739cd17bc5056c66
Adeline Hillier <adelineh2000@gmail.com>
2021-03-14 18:49:25 -0700

    Minor misspellings

--- src/loss_functions.jl
+++ src/loss_functions.jl
@@ -16,7 +16,7 @@ struct LossFunction{R, F, W, T, P}
 end
 
 function LossFunction(model, data; fields,
-                          targets = 1:length(data.t), 
+                          targets = 1:length(data.t),
                           weights = nothing,
                       time_series = TimeSeriesAnalysis(data.t[targets], TimeAverage()),
                           profile = ValueProfileAnalysis(model.grid)
@@ -53,12 +53,12 @@ struct TimeAverage end
 """
     struct ValueProfileAnalysis{D, A}
 
-A type for doing analyses on a discrepency profile located
+A type for doing analyses on a discrepancy profile located
 at cell centers. Defaults to taking the mean square difference between
 the model and data coarse-grained to the model grid.
 """
 struct ValueProfileAnalysis{D, A}
-    discrepency :: D
+    discrepancy :: D
        analysis :: A
 end
 
@@ -66,25 +66,25 @@ ValueProfileAnalysis(grid; analysis=mean) = ValueProfileAnalysis(CellField(grid)
 ValueProfileAnalysis(; analysis=mean) = ValueProfileAnalysis(nothing, analysis)
 on_grid(profile::ValueProfileAnalysis, grid) = ValueProfileAnalysis(grid; analysis=profile.analysis)
 
-function calculate_value_discrepency!(value, model_field, data_field)
-    coarse_grained = discrepency = value.discrepency
+function calculate_value_discrepancy!(value, model_field, data_field)
+    coarse_grained = discrepancy = value.discrepancy
     set!(coarse_grained, data_field)
 
-    for i in eachindex(discrepency)
-        @inbounds discrepency[i] = (coarse_grained[i] - model_field[i])^2
+    for i in eachindex(discrepancy)
+        @inbounds discrepancy[i] = (coarse_grained[i] - model_field[i])^2
     end
     return nothing
 end
 
 """
-    analyze_profile_discrepency(value, model_field, data_field)
+    analyze_profile_discrepancy(value, model_field, data_field)
 
-Calculates the discrepency between model and data field values, and returns an
-analysis of the discrepency profile.
+Calculates the discrepancy between model and data field values, and returns an
+analysis of the discrepancy profile.
 """
-function analyze_profile_discrepency(value, model_field, data_field)
-    calculate_value_discrepency!(value, model_field, data_field)
-    return value.analysis(value.discrepency)
+function analyze_profile_discrepancy(value, model_field, data_field)
+    calculate_value_discrepancy!(value, model_field, data_field)
+    return value.analysis(value.discrepancy)
 end
 
 """
@@ -107,22 +107,22 @@ GradientProfileAnalysis(grid; analysis=mean, gradient_weight=1.0, value_weight=1
     GradientProfileAnalysis(CellField(grid), FaceField(grid), FaceField(grid),
                             gradient_weight, value_weight, analysis)
 
-GradientProfileAnalysis(; analysis=mean, gradient_weight=1.0, value_weight=1.0) = 
+GradientProfileAnalysis(; analysis=mean, gradient_weight=1.0, value_weight=1.0) =
     GradientProfileAnalysis(nothing, nothing, nothing, gradient_weight, value_weight, analysis)
 
 function on_grid(profile::GradientProfileAnalysis, grid)
-    return GradientProfileAnalysis(grid;        
-                                          analysis = profile.analysis, 
+    return GradientProfileAnalysis(grid;
+                                          analysis = profile.analysis,
                                    gradient_weight = profile.gradient_weight,
                                       value_weight = profile.value_weight)
 end
-                            
-function calculate_gradient_discrepency!(prof, model_field, data_field)
+
+function calculate_gradient_discrepancy!(prof, model_field, data_field)
     # Coarse grain the data
     ϵ = prof.ϵ
     set!(ϵ, data_field)
 
-    # Calculate profients of both data and discrepency
+    # Calculate profients of both data and discrepancy
     ∇ϕ = prof.∇ϕ
     ∇ϵ = prof.∇ϵ
     ∂z!(∇ϵ, ϵ)
@@ -130,24 +130,24 @@ function calculate_gradient_discrepency!(prof, model_field, data_field)
 
     for i in eachindex(ϵ)
         @inbounds ϵ[i] = (ϵ[i] - model_field[i])^2
-        @inbounds ∇ϵ[i] = (∇ϵ[i] - ∇ϕ[i])^2 # incluϵes bottom boundary value, which will later be ignored.
+        @inbounds ∇ϵ[i] = (∇ϵ[i] - ∇ϕ[i])^2 # includes bottom boundary value, which will later be ignored.
     end
 
     # Top boundary contribution (ignored for now)
     #N = d.grid.N
-    #@inbounds ∇d[N+1] = (∇d[N+1] - ∇ϕ[N+1])^2 
+    #@inbounds ∇d[N+1] = (∇d[N+1] - ∇ϕ[N+1])^2
 
     return nothing
 end
 
 """
-    analyze_profile_discrepency(prof::GradientProfileAnalysis, model_field, data_field)
+    analyze_profile_discrepancy(prof::GradientProfileAnalysis, model_field, data_field)
 
 Calculates the discrepencies between both values and gradients of model and data fields,
-and returns an analysis of the two discrepency profiles.
+and returns an analysis of the two discrepancy profiles.
 """
-function analyze_profile_discrepency(prof::GradientProfileAnalysis, model_field, data_field)
-    calculate_gradient_discrepency!(prof, model_field, data_field)
+function analyze_profile_discrepancy(prof::GradientProfileAnalysis, model_field, data_field)
+    calculate_gradient_discrepancy!(prof, model_field, data_field)
 
     # Calculate analysis on gradient, excluding boundary points.
     return prof.analysis(prof.ϵ) + prof.gradient_weight * prof.analysis(prof.∇ϵ.data[2:end-1])
@@ -160,8 +160,8 @@ end
 @inline get_weight(::Nothing, field_index) = 1
 @inline get_weight(weights, field_index) = @inbounds weights[field_index]
 
-function analyze_weighted_profile_discrepency(loss, model, data, target)
-    total_discrepency = zero(eltype(model.grid))
+function analyze_weighted_profile_discrepancy(loss, model, data, target)
+    total_discrepancy = zero(eltype(model.grid))
     field_names = Tuple(loss.fields)
 
     for (field_index, field_name) in enumerate(field_names)
@@ -169,27 +169,27 @@ function analyze_weighted_profile_discrepency(loss, model, data, target)
         data_field = getproperty(data, field_name)[target]
 
         # Calculate the per-field profile-based disrepency
-        field_discrepency = analyze_profile_discrepency(loss.profile, model_field, data_field)
+        field_discrepancy = analyze_profile_discrepancy(loss.profile, model_field, data_field)
 
-        # Accumulate weighted profile-based disrepencies in the total discrepencyor
-        total_discrepency += get_weight(loss.weights, field_index) * field_discrepency # accumulate discrepencyor
+        # Accumulate weighted profile-based disrepencies in the total discrepancyor
+        total_discrepancy += get_weight(loss.weights, field_index) * field_discrepancy # accumulate discrepancyor
     end
 
-    return nan2inf(total_discrepency)
+    return nan2inf(total_discrepancy)
 end
 
 function evaluate!(loss, parameters, model_plus_Δt, data)
 
     # Initialize
     initialize_forward_run!(model_plus_Δt, data, parameters, loss.targets[1])
-    @inbounds loss.time_series.data[1] = analyze_weighted_profile_discrepency(loss, model_plus_Δt, data, 1)
+    @inbounds loss.time_series.data[1] = analyze_weighted_profile_discrepancy(loss, model_plus_Δt, data, loss.targets[1])
 
     # Calculate a loss function time-series
     for (i, target) in enumerate(loss.targets)
         run_until!(model_plus_Δt.model, model_plus_Δt.Δt, data.t[target])
 
-        @inbounds loss.time_series.data[i] = 
-            analyze_weighted_profile_discrepency(loss, model_plus_Δt, data, target) 
+        @inbounds loss.time_series.data[i] =
+            analyze_weighted_profile_discrepancy(loss, model_plus_Δt, data, target)
     end
 
     return nothing
